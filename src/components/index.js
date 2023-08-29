8afaa9c11fe67627319a42a58f15e8efda95cb9a
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-29 18:25:53 +0200

    exports

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './Button';
+export * from './ImageGallery';
+export * from './ImageGalleryItem';
+export * from './Loader';
+export * from './Modal';
+export * from './Searchbar';
+export * from './SearchForm';
